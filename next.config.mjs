4eeb4e2eebfd3cec07b0a16905d7ede945b2bf60
nik03072005 <nc03072005@gmail.com>
2025-09-29 12:45:27 +0530

    fix: update remote image hostname in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,13 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'nirmal.s3.ap-south-1.amazonaws.com',
+        hostname: 'libkart.s3.ap-south-1.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+       {
+        protocol: 'https',
+        hostname: 'kamargaoncollege.s3.ap-south-1.amazonaws.com',
         port: '',
         pathname: '/**',
       },
